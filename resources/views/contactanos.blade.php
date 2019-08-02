3989c7504883fe9cd1542bcf380286c885af1bec
Kevin Beltráń <cifuentes.kevin@correounivalle.edu.co>
2019-08-01 21:38:38 -0500

    terminación de la vista contactanos

--- resources/views/contactanos.blade.php
+++ resources/views/contactanos.blade.php
@@ -56,7 +56,7 @@
             <!-- Sección de Redes Sociales-->
             <div class="container">
                 <div class="row">
-                    <div class="col p-3 ">
+                    <div class="col p-3 ml-5">
                    
                     <img id="wpp" class="ml-5" src="{{ asset('img/redes-sociales/facebook.png') }}" alt="Facebook" width="100">
                     
@@ -74,7 +74,7 @@
                           
                     </div>
 
-                    <div class="col ml-5 p-3">
+                    <div class="col mr-5 p-3">
 
                     <img class="ml-5 mr-0" src="{{ asset('img/redes-sociales/whatsapp.png') }}" alt="Whatsapp" width="100">          
                     
@@ -85,61 +85,53 @@
             </div>
     <!-- Fin sección Redes sociales-->
 
-    <!-- Misión y visión -->
-    <div id="misi_visi" data-aos="fade-up" data-aos-duration="1500" >
-        <div class="row">
-            <div class="col p-3">
-                <h2>Misión</h2>
-                </div>
-            <div class="col p-3">
-                <h2>Visión</h2>
+    <!-- Ubicación -->
+  <!--  <h2 class="row justify-content-center subtitulo-DS pt-3">Ubicación</h2>
+            <div id="separador" class="row align-items-center row justify-content-center">
+                <hr align="left" class="izquierda-DS ml-0 mr-2" noshade="noshade" style="width:200px;">✻
+                <hr align="left" class="derecha-DS ml-2 mr-0" noshade="noshade" style="width:200px;"php >
             </div>
+        -->
+            <div class=" p-3 ubicacion">
+                <div class="row">
+                    <div class="col pt-3">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d254900.13120968675!2d-76.66575507688069!3d3.395397032954704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a6f0cc4bb3f1%3A0x1f0fb5e952ae6168!2sCali%2C+Valle+del+Cauca!5e0!3m2!1ses!2sco!4v1564714432895!5m2!1ses!2sco" width="600" height="462" frameborder="0" style="border:0" allowfullscreen></iframe>
 
-            <div class="w-100"></div>
+                    </div>
+                    <div id="texto-mapa" class="col pt-3 pb-3">
+                        <h3 class="pb-3">Ubicados en Cali, Colombia. Realizamos envíos a todo el país</h3>
+                        <!-- Default form subscription -->
+                        <form class="text-center  bg-success form-ubicacion border-light p-4" action="#!">
 
-            <div class="col">
-                <P class="pl-4"> Somos una empresa dedicada a la producción y comercialización de plantas Carnívoras,
-                    que mediante la investigación y pasión por este tipo de plantas,
-                    buscamos la concientización de importancia de estos seres vivos en los ecosistemas.</P>
-            </div>
-            <div class="col">
-                <P class="pr-4"> Ser en el 2023 la empresa líder en producción y comercialización de Plantas Carnívoras en Colombia,
-                    caracterizándose por la calidad de cultivo y servicio,
-                    haciendo uso de las buenas prácticas medioambientales.
-                </P>
-            </div>
+                        <p class="h4 mb-4">Escríbenos</p>
+
+                        <p>Puedes enviarnos una recomendación, queja, reclamo o lo que desees!</p>
+
+                    
+
+                        <!-- Name -->
+                        <input type="text" id="defaultSubscriptionFormPassword" class="form-control form-ubicacion mb-4" placeholder="Name">
+
+                        <!-- Email -->
+                        <input type="email" id="defaultSubscriptionFormEmail" class="form-control form-ubicacion bicacion mb-4" placeholder="E-mail">
+                        
+                        <!-- Textp -->
+                        <input type="text-area" id="texto" class="form-control form-ubicacion bicacion mb-4" placeholder="Tu mensaje">
+
+                        <!-- Sign in button -->
+                        <button class="btn btn-block" type="submit">Enviar</button>
 
 
-        </div>
-
-    </div>
-    <!-- Fin Misión y visión -->
-
-
-
-    <!-- Valores-->
-
-    <h2 class="p-3">Valores</h2>
-    <div class="container">
-            <div class="row">
-                <div class="col pb-5 col-md-7">
-                        <ul class="list-group lista">
-                                <li class="list-group-item">Espíritu constructivo</li>
-                                <li class="list-group-item resaltar" data-aos="fade-right" data-aos-duration="2000">Profesionalismo</li>
-                                <li class="list-group-item">Honestidad</li>
-                                <li class="list-group-item resaltar"  data-aos="fade-right" data-aos-duration="2000">Compromiso</li>
-                                <li class="list-group-item">Responsabilidad</li>
-                                <li class="list-group-item resaltar"  data-aos="fade-right" data-aos-duration="2000">Enfoque en la mejora continua</li>
-                            </ul>
-                </div>
-                <div class="col pl-5 ml-5 ">
-                        <img class="pb-5" src="{{ asset('img/quienes_somos/valores.png') }}" alt="Valores corporativos" width="300">
-
+</form>
+<!-- Default form subscription -->
+                    </div>
                 </div>
             </div>
+    <!-- Fin Ubicación -->
 
-        </div>
 
+
+    
 
 
 
