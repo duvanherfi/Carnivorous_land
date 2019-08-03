6ff09c27778836c4cc4e99d1cbf68bdc98317631
Andres Felipe <andres.garcia.gaviria@correounivalle.edu.co>
2019-08-03 16:11:38 -0500

    Diseño de la carta

--- resources/js/components/cartas_productos.vue
+++ resources/js/components/cartas_productos.vue
@@ -5,30 +5,88 @@
     <div class="card m-3 carta-DS" style="width: 19rem; height: 370px;">
         <!--Card image-->
         <div class="view overlay">
-            <img class="imagen-DS" src="" alt="">
-            <div class="mask rgba-white-slight"></div>
-        </div>
-    </div>
-    <!--/.Card-->
-    <!--Card-->
-    <div class="card m-3 carta-DS" style="width: 19rem; height: 370px;">
-        <!--Card image-->
-        <div class="view overlay">
             <img class="imagen-producto-DS shadow-lg mb-5 bg-white" src="/img/cartas/carta1.jpg" alt="">
             <div class="mask rgba-white-slight"></div>
         </div>
-    </div>
-    <!--/.Card-->
-    <!--Card-->
-    <div class="card m-3 carta-DS" style="width: 19rem; height: 370px;">
-        <!--Card image-->
-        <div class="view overlay">
-            <img class="imagen-DS" src="" alt="">
-            <div class="mask rgba-white-slight"></div>
+        <!-- /Imagen -->
+        <!-- Contenido -->
+        <div class="card-body card-body-cascade">
+            <div class="row row justify-content-between">
+                <!-- Label -->
+                <h5 class="col-5 font-weight-bold card-title mb-1">Drosera</h5>
+                <h6 class="col-5 align-self-center mb-1 px-3">Tamaño: M</h6>
+            </div>
+            <!-- Title -->
+            <h6 class="mb-1">Capensis</h6>
+            <!-- <form> -->
+            <p class="clasificacion">
+                <input id="radio1" type="radio" name="estrellas" value="5">
+                <label for="radio1">★</label>
+                <input id="radio2" type="radio" name="estrellas" value="4">
+                <label for="radio2">★</label>
+                <input id="radio3" type="radio" name="estrellas" value="3">
+                <label for="radio3">★</label>
+                <input id="radio4" type="radio" name="estrellas" value="2">
+                <label for="radio4">★</label>
+                <input id="radio5" type="radio" name="estrellas" value="1">
+                <label for="radio5">★</label>
+            </p>
+            <!-- </form> -->
+            <!-- Text -->
+            <p class="font-weight-bold card-text mb-1">$50.000 COP</p>
+            <!-- Button -->
+            <a class="btn btn-unique btn-block"><i class="fas fa-cart-plus"></i> Añadir al carro</a>
+            <!-- /Contenido -->
         </div>
+        <!--/.Card-->
+
     </div>
-    <!--/.Card-->
 </div>
 <!--/Cartas-->
 </template>
 
+<style>
+#form {
+    width: 250px;
+    margin: 0 auto;
+    height: 50px;
+}
+
+#form p {
+    text-align: center;
+}
+
+#form label {
+    font-size: 20px;
+}
+
+input[type="radio"] {
+    display: none;
+}
+
+label {
+    color: grey;
+}
+
+.clasificacion {
+    direction: rtl;
+    unicode-bidi: bidi-override;
+    width: 85px;
+    height: 30px;
+    margin: 0;
+}
+
+p {
+    cursor: default;
+}
+
+label:hover,
+label:hover~label {
+    color: orange;
+    cursor: pointer;
+}
+
+input[type="radio"]:checked~label {
+    color: orange;
+}
+</style>
--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -22,11 +22,11 @@
     <!-- Libreria AOS js -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <!-- Scripts -->
-    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
 
     <!-- Styles -->
-    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Libreria AOS css-->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <!-- Font Awesome -->
