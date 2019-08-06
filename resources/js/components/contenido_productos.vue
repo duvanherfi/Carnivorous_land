86d86c790529e6ef1a2c9a744f65b70dfd4adcf1
Andres Felipe <andres.garcia.gaviria@correounivalle.edu.co>
2019-08-05 22:13:01 -0500

    Se desarrollo parte de productos

--- resources/js/components/contenido_productos.vue
+++ resources/js/components/contenido_productos.vue
@@ -1,26 +1,34 @@
 <template>
-    <div class="contenido-productos-DS">
-        <img class="imagen-subtitulo-DS" src="/img/productos/fondo_productos.jpg" alt="Fondo productos">
-        <h1>Sarracenia</h1>
+<div class="contenido-productos-DS">
+    <div class="position-absolute subtitulo-producto-DS">
+        <h1>SARRACENIA</h1>
     </div>
+    <img class="imagen-subtitulo-DS" src="/img/productos/fondo_productos.jpg" alt="Fondo productos">
+    <div class="mask rgba-black-strong"></div>
+    <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, quas eaque veniam harum nostrum non pariatur! Non quidem adipisci pariatur eius provident impedit tenetur, quibusdam molestias culpa doloribus quam aliquam necessitatibus? Hic vel dolor quae ad veniam totam tempore culpa, omnis aut et doloremque laudantium. Incidunt labore alias libero aspernatur exercitationem sit tempora modi aliquid nesciunt ab atque minima, quos, dignissimos eos harum voluptates dicta! Vero eaque qui odit quo. Tempora et enim officiis dolore, eaque minima, quos voluptatum rerum velit officia assumenda praesentium tenetur quidem autem commodi culpa natus laudantium! Cupiditate voluptas est iure cumque enim perspiciatis id quasi nisi recusandae aliquam, labore numquam quod fugit temporibus distinctio omnis! Laboriosam explicabo consequatur nisi vitae neque tempore architecto blanditiis autem voluptatum sunt itaque magnam asperiores debitis dolor odit, accusantium nobis nulla fugiat totam odio reiciendis veritatis modi velit? Est accusantium blanditiis autem praesentium culpa? Similique libero quia natus est error voluptas sed et nobis ab sint accusantium, esse provident a in temporibus voluptate. Iure incidunt corporis dolorem aliquam quia rerum sit laudantium, debitis culpa maxime voluptatum doloribus, necessitatibus optio ea sequi alias laboriosam neque temporibus quis possimus inventore iusto aut impedit nemo. Amet nam iste at? Atque magni itaque quasi?</p>
+    <div>
+        <productos></productos>
+    </div>
+</div>
 </template>
 
 <style scoped>
-.imagen-subtitulo-DS{
+.imagen-subtitulo-DS {
     width: 960px;
     height: 150px;
     object-fit: cover;
 }
 
-.contenido-productos-DS{
+.contenido-productos-DS {
     margin-top: 1.5rem;
     margin-left: 2.3rem;
     margin-bottom: 1.5rem;
+    max-width: 960px;
 }
 
-h1{
-    z-index: 2;
-    widows: 500px;
+.subtitulo-producto-DS {
+    top: 34%;
+    left: 53%;
+    color: white;
 }
 </style>
-
