ecaf9d9fe28d8524b38e7524d35e8bd3955df061
DUVAN HERNANDEZ FIGUEROA <duvanherfi@gmail.com>
2019-08-08 07:42:24 -0500

    menu en vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,8 +12,13 @@ Vue.component('productos', require('./components/cartas_productos.vue').default)
 Vue.component('menu-producto-tips', require('./components/menu_productos_tips.vue').default);
 Vue.component('contenido-productos', require('./components/contenido_productos.vue').default);
 Vue.component('filas-carrito', require('./components/filas_carrito.vue').default);
+Vue.component('menu_prueba', require('./components/menu_prueba.vue').default);
 
 
 const app = new Vue({
     el: '#app',
+});
+
+const app2 = new Vue({
+    el: '#app2',
 });
---
+++ resources/js/components/menu_prueba.vue
@@ -0,0 +1,66 @@
+<template>
+    <ul class="navbar-nav ml-auto nav-flex-icons bordes-menu-DS">
+        <li class="nav-item opcion-menu-DS bordes-DS">
+            <a class="nav-link waves-effect waves-light" v-bind:href="inicio">
+                INICIO
+            </a>
+        </li>
+
+        <li class="nav-item bordes-DS">
+            <a class="nav-link waves-effect waves-light" v-bind:href="quienes_somos">
+                ¿QUIÉNES SOMOS?
+            </a>
+        </li>
+        <li class="nav-item dropdown opcion-menu-DS bordes-DS" v-if="1>0">
+                <a class="nav-link dropdown-toggle" v-bind:href="producto" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        PRODUCTOS
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right dropdown-default col-4 minimenu-DS"
+                        aria-labelledby="navbarDropdownMenuLink-333">
+                        <a class="dropdown-item" v-bind:href="plantas">Plantas</a>
+                        <a class="dropdown-item" v-bind:href="imple_cultivo">Implementos de cultivo</a>
+                        <a class="dropdown-item" v-bind:href="merchandising">Merchandising</a>
+                    </div>
+                </li>
+
+                <li class="nav-item opcion-menu-DS bordes-DS" v-else>
+                    <a class="nav-link waves-effect waves-light" v-bind:href="inventario">
+                        INVENTARIO
+                    </a>
+                </li>
+
+                <li class="nav-item opcion-menu-DS bordes-DS">
+                    <a class="nav-link waves-effect waves-light" v-bind:href="tips_cultivo">
+                        TIPS DE CULTIVOS
+                    </a>
+                </li>
+                <li class="nav-item opcion-menu-DS">
+                    <a class="nav-link waves-effect waves-light" v-bind:href="contactanos">
+                        CONTÁCTANOS
+                    </a>
+                </li>
+            </ul>
+
+    </template>
+
+<script>
+export default {
+    data() {
+        return {
+            inicio: route('inicio'),
+            quienes_somos: route('quienes_somos'),
+            producto:null,
+            plantas:route('productos_plantas'),
+            imple_cultivo:null,
+            merchandising:null,
+            inventario:null,
+            tips_cultivo:null,
+            contactanos:route('contactanos')
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
